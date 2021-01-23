e081d257314bfcfc19fa314459490d33f98b08ca
zainabalazzawi <zainabalazzawi91@gmail.com>
2021-01-23 10:21:05 +0300

    add validation/shipping

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,78 +1,136 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
-// import { useForm } from "./useForm";
+import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!values.email.includes("@")) {
+    errors.email = "invalid email";
+  }
+  if (!values.FirstName) {
+    errors.FirstName = "Enter a first name";
+  }
+  if (!values.LastName) {
+    errors.LastName = "Enter a last name";
+  }
+  if (!values.Adress) {
+    errors.Adress = "Enter an address";
+  }
+  if (!values.City) {
+    errors.City = "Enter a city";
+  }
+  if (!values.Phone) {
+    errors.Phone = "Enter a valid phone number";
+  }
+  return errors;
+};
 
 export default function shipping() {
-  // const [values, handleChange, handleSubmit] = useForm({
-  //   FirstName: "",
-  //   LastName: "",
-  //   Adress: "",
-  //   Apartment: "",
-  //   City: "",
-  //   Phone: "",
-  // });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      FirstName: "",
+      LastName: "",
+      Adress: "",
+      Apartment: "",
+      City: "",
+      Phone: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const { touched, errors } = formik;
   return (
     <Wrapper>
-      <ContactWrapper>
-        <h2>Contact information </h2>
-        <span>
-          Already have an account? &nbsp;
-          <Link href="./form">Log in</Link>
-        </span>
+      <form onSubmit={formik.handleSubmit}>
+        <ContactWrapper>
+          <h2>Contact information </h2>
+          <span>
+            Already have an account? &nbsp;
+            <Link href="./form">Log in</Link>
+          </span>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            {...formik.getFieldProps("email")}
+          />
+          {errors.email && touched.email ? (
+            <span style={{ fontSize: 15, color: "red" }}>{errors.email}</span>
+          ) : null}
+          <label>
+            <input type="checkbox" />
+            Keep me up to date on news and exclusive offers
+          </label>
+        </ContactWrapper>
+        <ShippingWrapper>
+          <h2>Shipping address</h2>
+          <input
+            type="First Name"
+            id="First Name"
+            placeholder="First Name"
+            {...formik.getFieldProps("First Name")}
+          />
+          {errors.FirstName && touched.FirstName ? (
+            <span style={{ fontSize: 15, color: "red" }}>
+              {errors.FirstName}
+            </span>
+          ) : null}
 
-        <input type="email" placeholder="Email" />
-        <label>
-          <input type="checkbox" />
-          Keep me up to date on news and exclusive offers
-        </label>
-      </ContactWrapper>
-      <ShippingWrapper>
-        <h2>Shipping address</h2>
-        <input
-          placeholder="First Name"
-          // name="First Name"
-          // type="text"
-          // value={values.FirstName}
-          // onChange={handleChange}
-        />
-        <input
-          placeholder="Last Name"
-          // name="FirstName"
-          // type="text"
-          // value={values.FirstName}
-          // onChange={handleChange}
-        />
-        <input
-          placeholder="Adress"
-          // name="Adress"
-          // type="text"
-          // value={values.Adress}
-          // onChange={handleChange}
-        />
-        <input
-          placeholder="Apartment"
-          // name="partment"
-          // type="text"
-          // value={values.Apartment}
-          // onChange={handleChange}
-        />
-        <input
-          placeholder="City"
-          // name="City"
-          // type="text"
-          // value={values.City}
-          // onChange={handleChange}
-        />
-        <input
-          placeholder="Phone"
-          // name="Phone"
-          // type="text"
-          // value={values.Phone}
-          // onChange={handleChange}
-        />
-        <button type="submit">Continue to shipping</button>
-      </ShippingWrapper>
+          <input
+            type="Last Name"
+            id="Last Name"
+            placeholder="Last Name"
+            {...formik.getFieldProps("Last Name")}
+          />
+          {errors.LastName && touched.LastName ? (
+            <span style={{ fontSize: 15, color: "red" }}>
+              {errors.LastName}
+            </span>
+          ) : null}
+
+          <input
+            type="Adress"
+            id="Adress"
+            placeholder="Adress"
+            {...formik.getFieldProps("Adress")}
+          />
+          {errors.Adress && touched.Adress ? (
+            <span style={{ fontSize: 15, color: "red" }}>{errors.Adress}</span>
+          ) : null}
+
+          <input placeholder="Apartment" />
+
+          <input
+            type="City"
+            id="City"
+            placeholder="City"
+            {...formik.getFieldProps("City")}
+          />
+          {errors.City && touched.City ? (
+            <span style={{ fontSize: 15, color: "red" }}>{errors.City}</span>
+          ) : null}
+
+          <input
+            type="Phone"
+            id="Phoney"
+            placeholder="Phone"
+            {...formik.getFieldProps("Phone")}
+          />
+          {errors.Phone && touched.Phone ? (
+            <span style={{ fontSize: 15, color: "red" }}>{errors.Phone}</span>
+          ) : null}
+
+          <button type="submit">Continue to shipping</button>
+        </ShippingWrapper>
+      </form>
     </Wrapper>
   );
 }
